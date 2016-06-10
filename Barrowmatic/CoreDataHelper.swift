07fa72dab7af883d394f85da58ce8ba366d8335d
Peyman Attarzadeh <peymanium@Peymans-Macbook.local>
2016-06-10 13:33:23 +0400

    User interface and CoreData helper functions created

---
+++ Barrowmatic/CoreDataHelper.swift
@@ -0,0 +1,78 @@
+//
+//  CoreDataHelper.swift
+//  Barrowmatic
+//
+//  Created by Peyman Attarzadeh on 6/10/16.
+//  Copyright © 2016 PeymaniuM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class CoreDataHelper
+{
+    static let helper = CoreDataHelper()
+    
+    
+    func ManagedObjectContext() -> NSManagedObjectContext
+    {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        return appDelegate.managedObjectContext
+    }
+    
+    
+    func InsertManagedObject (className : NSString, managedObjectContext : NSManagedObjectContext) -> AnyObject
+    {
+        let managedObject = NSEntityDescription.insertNewObjectForEntityForName(className as String, inManagedObjectContext: managedObjectContext)
+        
+        return managedObject
+    }
+    
+    
+    func FetchEntities(className : NSString, managedObjectContext : NSManagedObjectContext, predicate : NSPredicate?, sortDescriptor : NSSortDescriptor?) -> NSArray //[AnyObject]
+    {
+        let fetchRequest = NSFetchRequest(entityName: className as String)
+        
+        fetchRequest.sortDescriptors = []
+        if sortDescriptor != nil
+        {
+            fetchRequest.sortDescriptors?.append(sortDescriptor!)
+        }
+        
+        if predicate != nil
+        {
+            fetchRequest.predicate = predicate
+        }
+        
+        
+        var items = []
+        do
+        {
+            items = try managedObjectContext.executeFetchRequest(fetchRequest)
+        }
+        catch let error as NSError
+        {
+            print (error.debugDescription)
+        }
+        
+        return items //as [AnyObject]
+        
+    }
+    
+    
+    func SaveManagedObjectContext(managedObjectContext : NSManagedObjectContext)
+    {
+        
+        do
+        {
+            try managedObjectContext.save()
+        }
+        catch let error as NSError
+        {
+            print (error.debugDescription)
+        }
+        
+    }
+    
+}
